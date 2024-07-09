f6b17c1fe570e9ad52101545a36ce83a7c6a3597
ASZhidenko <aszhidenko@mai.education>
2024-07-09 19:50:27 +0300

    исправление отсылки message на chat и chat на message

--- backend/internal/api/message/create_message.go
+++ backend/internal/api/message/create_message.go
@@ -9,9 +9,10 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func CreateMessage(ctx echo.Context, req models.MessageToCreate, httpReq *http.Request) (models.Message, error) {
+func CreateMessage(ctx echo.Context, req models.MessageToCreate, chatID uint, httpReq *http.Request) (models.Message, error) {
 	date := time.Now()
 	dbMessage := req.ToDB(date)
+	dbMessage.ChatID = chatID
 	dbRequest := storage.GetDB().Create(dbMessage)
 	if dbRequest.Error != nil {
 		return models.Message{}, dbRequest.Error
